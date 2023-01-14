e0454d18cb9add878ce429fe3587885475d68e8d
timonforrer <timon.forrer@gmail.com>
2023-01-14 21:30:11 +0100

    Install localisation plugin

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {withDocumentI18nPlugin, getDocumentList} from '@sanity/document-internationalization'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,25 @@ export default defineConfig({
   projectId: '0j5eyb7h',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: withDocumentI18nPlugin((pluginConfig) => ([
+    deskTool({
+      structure: (S, {schema}) => getDocumentList({S, schema, config: pluginConfig}),
+    }),
+    visionTool(),
+  ]), {
+    includeDeskTool: false,
+    languages: [
+      {
+        "id": "de",
+        "title": "Deutsch"
+      },
+      {
+        "id": "en",
+        "title": "English"
+      }
+    ],
+    idStructure: "subpath"
+  }),
 
   schema: {
     types: schemaTypes,
