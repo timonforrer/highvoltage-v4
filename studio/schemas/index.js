039566cbd2467d6f2f892c45396bba391c274ed6
timonforrer <timon.forrer@gmail.com>
2023-01-15 00:48:12 +0100

    Hook up new schemas

--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -1,15 +1,32 @@
-import gig from "./documents/gig";
-import modular from "./documents/modular";
-import release from "./documents/release";
+import providers from "./refs/providers";
+import reviews from "./refs/reviews";
+import tracks from "./refs/tracks";
+import videos from "./refs/videos";
+import arrayRichtext from "./object/arrayRichtext";
 import gigs from "./object/gigs";
+import inlinevideo from "./object/inlinevideo";
 import meta from "./object/meta";
+import richtext from "./object/richtext";
+import video from "./object/video";
+import gig from "./pageTypes/gig";
+import modular from "./pageTypes/modular";
+import release from "./pageTypes/release";
 
 export const schemaTypes = [
   // objects
+  arrayRichtext,
   gigs,
+  inlinevideo,
   meta,
-  // documents
+  richtext,
+  video,
+  // page types
   gig,
   modular,
-  release
+  release,
+  // refs
+  providers,
+  reviews,
+  tracks,
+  videos,
 ]
