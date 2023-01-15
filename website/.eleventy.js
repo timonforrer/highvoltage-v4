4f92a804eb984e4ccfa278904d447bfaf2eda6c1
timonforrer <timon.forrer@gmail.com>
2023-01-15 14:13:18 +0100

    Add basic eleventy config

---
+++ website/.eleventy.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+// const { EleventyServerlessBundlerPlugin, EleventyRenderPlugin } = require('@11ty/eleventy');
+const WebC = require('@11ty/eleventy-plugin-webc');
+// const getLangs = require('./src/utils/getLangs');
+// const portableTextToHTML = require('./src/utils/portableTextToHTML');
+const sass = require('sass');
+const path = require('path');
+
+/**
+ * @param {import("@11ty/eleventy/src/UserConfig")} config 
+ */
+
+module.exports = function(config) {
+
+  // config.addPlugin(EleventyServerlessBundlerPlugin, {
+  //   name: 'preview',
+  //   functionsDir: './netlify/functions',
+  //   copy: ['src/utils']
+  // });
+
+  config.addPlugin(WebC, {
+    components: 'src/components/*.webc'
+  });
+
+  // config.addJavaScriptFunction('getLangs', getLangs);
+  // config.addJavaScriptFunction('portableTextToHTML', portableTextToHTML);
+
+  config.addTemplateFormats('scss');
+  config.addExtension('scss', {
+    outputFileExtension: 'css',
+    compile: async function(input, inputPath) {
+      let parsed = path.parse(inputPath);
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+      let result = sass.compileString(input);
+      return async (data) => result.css;
+    }
+  })
+
+  return {
+    dir: {
+      data: 'data',
+      includes: 'components',
+      input: 'src',
+      layouts: 'layouts',
+      output: 'dist'
+    }
+  }
+}
