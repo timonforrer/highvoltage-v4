90173fdf7014004724ae129db1f08806743e251e
timonforrer <timon.forrer@gmail.com>
2023-01-22 18:35:06 +0100

    Refactor the code to be more terse

--- website/api/populate-gigs.js
+++ website/api/populate-gigs.js
@@ -1,23 +1,19 @@
 // import clients
-const Sanity = require('@sanity/client');
-const Airtable = require('airtable');
+import Sanity from '@sanity/client';
+import Airtable from 'airtable';
+
+// import client configuration
+import {airtableConfig, sanityConfig} from '../config/clients';
+
 // import helper function for handling queries to the airtable database
-import { airtableFetch } from '../utils/airtableFetch';
+import {airtableFetch} from '../utils/airtableFetch';
 
 export default async function handler(req, res) {
 
   // configure clients
-  const sanity = Sanity({
-    projectId: '0j5eyb7h',
-    dataset: 'production',
-    apiVersion: '2023-01-16',
-    token: process.env.SANITY_IMPORT,
-    useCdn: false
-  });
-  Airtable.configure({
-    endpointUrl: 'https://api.airtable.com',
-    apiKey: process.env.AIRTABLE_API_KEY
-  });
+  const sanity = Sanity(sanityConfig);
+
+  Airtable.configure(airtableConfig);
   const airtable = Airtable.base(process.env.AIRTABLE_API_BASE_MASTER);
 
   // get all gigs from airtable
@@ -26,17 +22,25 @@ export default async function handler(req, res) {
     view: 'Default',
     filter: 'Status="Definitiv"'
   });
-  // only extract relevant fields
-  airtableGigs = airtableGigs.map(item => {
-    let { fields } = item;
-    return {
-      slug: fields['Slug'],
-      finalSlug: `/konzerte/${fields['Slug']}`,
-      id: fields['Id'],
-      name: fields['Name'],
-      startDate: fields['Start'],
-    };
-  });
+
+  // prepare fields for creation in sanity
+  airtableGigs = airtableGigs.map(({ fields: { Slug: slug, Id: id, Name: name, Start: start } }) => (
+    {
+      __i18n_lang: 'de',
+      _type: 'gig',
+      meta: {
+        title: name,
+        slug: {
+          _type: 'slug',
+          current: `/konzerte/${slug}`
+        }
+      },
+      internal: {
+        airtableId: id,
+        startDate: start.split('T')[0]
+      }
+    })
+  );
 
   // sanity query
   const sanityQuery = `*[_type=="gig"]{
@@ -44,48 +48,22 @@ export default async function handler(req, res) {
       airtableId
     }
   }`;
+
   // get gigs from sanity
   let sanityGigs = await sanity.fetch(sanityQuery, {});
 
+  // filter out any gig, that is present in airtable and in the sanity database
   const onlyInAirtable = airtableGigs.filter(airtableValue => {
-    return !sanityGigs.some(sanityValue => {
-      return airtableValue.id === sanityValue.internal?.airtableId
-    });
+    return !sanityGigs.find(({ internal: { airtableId } }) => airtableValue.internal.airtableId === airtableId);
   });
 
-  // function to map airtable object to sanity object
-  const transform = (airtableInput) => {
-    return {
-      __i18n_lang: 'de',
-      _type: 'gig',
-      meta: {
-        title: airtableInput.name,
-        slug: {
-          _type: 'slug',
-          current: airtableInput.finalSlug
-        }
-      },
-      internal: {
-        airtableId: airtableInput.id,
-        startDate: airtableInput.startDate.split('T')[0]
-      }
-    }
-  }
-
-  // transform the documents
-  let transformedDocuments = onlyInAirtable.map(doc => transform(doc));
-
-  // write to sanity
+  // prepare transactions for any document, that is missing in sanity
   let transaction = sanity.transaction();
-  transformedDocuments.forEach(doc => {
+  onlyInAirtable.forEach(doc => {
     transaction.create(doc);
   });
-  
-  // sanity
-  //   .delete({query: '*[_type == "gig"]'})
-  //   .then(() => res.status(200).json({message: 'deleted!'}))
-  //   .catch(err => res.status(500).json({err}))
 
+  // execute transaction
   transaction
     .commit()
     .then(() => {
