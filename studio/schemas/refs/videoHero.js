f5f0aa94b03a1dfbc13b4dd5c6af849bece36cb3
timonforrer <timon.forrer@gmail.com>
2023-01-30 20:50:16 +0100

    Add videoHero

---
+++ studio/schemas/refs/videoHero.js
@@ -0,0 +1,36 @@
+export default {
+  name: 'videoHero',
+  title: 'Video Hero',
+  type: 'document',
+  i18n: true,
+  initialValue: {
+    __i18n_lang: 'de',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'string'
+    },
+    {
+      name: 'description',
+      type: 'richtext'
+    },
+    {
+      name: 'cta',
+      title: 'Call to Action',
+      type: 'string'
+    },
+    {
+      name: 'video',
+      type: 'video'
+    },
+    {
+      name: 'backgroundImage',
+      title: 'Background Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    }
+  ],
+}
