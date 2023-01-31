06061759fd21027ce9722b4587d52a5af9f329b0
timonforrer <timon.forrer@gmail.com>
2023-01-31 23:52:48 +0100

    Add credits

---
+++ studio/schemas/refs/credits.js
@@ -0,0 +1,16 @@
+export default {
+  name: 'credits',
+  title: 'Credits',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+    },
+    {
+      name: 'url',
+      title: 'URL',
+      type: 'url',
+    },
+  ],
+}
