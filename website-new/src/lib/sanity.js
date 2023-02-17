d3c6e112d3eb1e272d741408cee640334fb88774
timonforrer <timon.forrer@gmail.com>
2023-02-17 14:39:21 +0100

    Add helper to get cropped images

--- website-new/src/lib/sanity.js
+++ website-new/src/lib/sanity.js
@@ -25,3 +25,54 @@ export async function getSanityData({query}) {
 };
 
 export const portableTextConfig = {}
+
+export const getWidthAndHeight = ({aspect, originalWidth, originalHeight, outputWidth}) => {
+  // aspect is specified as '16:9'
+  const [aspectX, aspectY] = aspect.split(':').map(Number);
+  // e.g. 0.5625 = 9 / 16
+  const aspectNum = aspectY / aspectX;
+  // calculate height based on width
+  const outputHeight = outputWidth * aspectNum;
+  // case 1: calculated width and height are both smaller than actual w & h
+  // all good, return as is
+  if (outputWidth <= originalWidth && outputHeight <= originalHeight) {
+    return {
+      height: Math.round(outputHeight),
+      width: Math.round(outputWidth),
+    }
+  }
+  // case 2: calculated height is larger than original height
+  // use original height to calculate new width
+  if (outputHeight > originalHeight) {
+    const newOutputWidth = originalHeight * aspectX / aspectY;
+    return {
+      height: Math.round(originalHeight),
+      width: Math.round(newOutputWidth),
+    }
+  }
+  // case 3: calculated width is larger than original width
+  // use original width to calculate new height
+  if (outputWidth > originalWidth) {
+    const newOutputHeight = originalWidth * aspectNum;
+    return {
+      height: Math.round(newOutputHeight),
+      width: Math.round(originalWidth),
+    }
+  }
+}
+
+export const getUrl = ({focalPointX, focalPointY, width, height, baseUrl}) => {
+
+  const url = new URL(baseUrl);
+
+  url.searchParams.set('w', width);
+  url.searchParams.set('h', height);
+
+  if (focalPointX !== undefined) {
+    url.searchParams.set('fit', 'crop');
+    url.searchParams.set('fp-x', focalPointX);
+    url.searchParams.set('fp-y', focalPointY);
+  }
+
+  return url;
+}
