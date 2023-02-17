12111448b5efaf821b96e6734f629cfb83c91853
timonforrer <timon.forrer@gmail.com>
2023-02-17 14:39:10 +0100

    Update query to consume productVariant image

--- website-new/src/lib/queries.js
+++ website-new/src/lib/queries.js
@@ -1,77 +1,84 @@
 export const modularPages = `
-  *[_type == "modular"]{
+*[_type == "modular"]{
+  ...,
+  blocks[] {
     ...,
-    blocks[] {
-      ...,
-      _type in [
-        "videoHero",
-        "section",
-        "productCategory"
-      ] => @-> {
-        _type == "gallery" => {
+    _type in [
+      "videoHero",
+      "section",
+      "productCategory"
+    ] => @-> {
+      _type == "gallery" => {
+        ...,
+        images[] {
           ...,
-          images[] {
-            ...,
-            credits->,
-            image {
-              ...,
-              asset->,
-            },
-          },
-        },
-        _type == "videoHero" => {
-          ...,
-          backgroundImage{
+          credits->,
+          image {
             ...,
             asset->,
-          },
-          backgroundVideo{
-            ...,
-            asset->,
-          },
-        },
-        _type == "section" => {
-          ...,
-          cta {
-            title,
-            "slug": page->meta.slug.current,
-          },
-          blocks[]{
-            _type == "reference" => @->{
-              ...,
-              _type == "release" => {
-                artwork {
-                  ...,
-                  asset->,
-                },
-                providers->,
-                tracks->{
-                  ...,
-                  items[] {
-                    ...,
-                    file {
-                      ...,
-                      asset->,
-                    },
-                  },
-                },
-              },
-              _type == "milestone" => {
-                ...,
-                image {
-                  ...,
-                  asset->,
-                },
-                cta {
-                  title,
-                  "slug": page->meta.slug.current,
-                },
-              },
-            },
-            _type == "internalCollection" => @,
           },
         },
       },
+      _type == "videoHero" => {
+        ...,
+        backgroundImage{
+          ...,
+          asset->,
+        },
+        backgroundVideo{
+          ...,
+          asset->,
+        },
+      },
+      _type == "section" => {
+        ...,
+        cta {
+          title,
+          "slug": page->meta.slug.current,
+        },
+        blocks[]{
+          _type == "reference" => @->{
+            ...,
+            _type == "release" => {
+              artwork {
+                ...,
+                asset->,
+              },
+              providers->,
+              tracks->{
+                ...,
+                items[] {
+                  ...,
+                  file {
+                    ...,
+                    asset->,
+                  },
+                },
+              },
+            },
+            _type == "milestone" => {
+              ...,
+              image {
+                ...,
+                asset->,
+              },
+              cta {
+                title,
+                "slug": page->meta.slug.current,
+              },
+            },
+            _type == "productVariant" => {
+              ...,
+              mainImage {
+                ...,
+                asset->
+              }
+            }
+          },
+          _type == "internalCollection" => @,
+        },
+      },
     },
-  }
+  },
+}
 `;
