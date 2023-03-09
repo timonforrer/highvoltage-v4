d6c1811ce76ac679c54044ef25a89adac049844f
timonforrer <timon.forrer@gmail.com>
2023-03-10 00:09:38 +0100

    Outsource population of sanitySkus with airtableSku data

--- website/src/lib/utils.js
+++ website/src/lib/utils.js
@@ -1,3 +1,28 @@
-export const replacer = ({string, data}) => string.replace(/{{\s*(\w+)\s*}}/g, (match, key) => {
+import { getAirtableData } from '../lib/airtable';
+import { getSanityData } from '../lib/sanity';
+import { productSkus } from '../lib/queries';
+
+const replacer = ({string, data}) => string.replace(/{{\s*(\w+)\s*}}/g, (match, key) => {
   return typeof data[key] !== 'undefined' ? data[key] : match;
 });
+
+const basicPopulatedSkus = async ({ baseSkus }) => {
+  const airtableSkus = await getAirtableData({
+    table: 'Artikel',
+    base: 'shop',
+    view: 'Online',
+  });
+
+  const skus = airtableSkus.filter(airtableSku => {
+    return baseSkus.some(sku => airtableSku['Artikelnummer'] === sku.sku);
+  });
+
+  return {
+    skus
+  }
+}
+
+export {
+  replacer,
+  basicPopulatedSkus
+}
