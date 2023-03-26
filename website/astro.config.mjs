727cdc0f5eb331dc75fcff60b09e8dc2935478f1
timonforrer <timon.forrer@gmail.com>
2023-03-26 20:46:19 +0200

    Add lit integration and sendgrid and update sp-form

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
+import lit from '@astrojs/lit';
 
 // https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    },
+    lit())],
   vite: {},
   server: {
     port: process.env.PORT || 3000,
