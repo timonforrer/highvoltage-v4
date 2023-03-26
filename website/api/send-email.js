6139805e8f2a3eafe12b27535e4dadb494800537
timonforrer <timon.forrer@gmail.com>
2023-03-26 20:46:42 +0200

    Add basic email api

---
+++ website/api/send-email.js
@@ -0,0 +1,14 @@
+// const sg = require('@sendgrid/mail');
+
+import { form as formQuery } from '../src/lib/queries.js';
+import { getSanityData } from '../src/lib/sanity.js';
+
+export default async function (req, res) {
+  // sg.setApiKey(process.env.VA_SENDGRID_API_KEY);
+  const { redirect, id } = req.body;
+  const form = await getSanityData({ query: formQuery, params: {$id: id} });
+  
+  
+  
+  res.redirect(redirect);
+}
