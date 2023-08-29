f3ce5e28f5265a5211a6dd0e68e8dee06edcbdb0
timonforrer <timon.forrer@gmail.com>
2023-08-29 22:50:13 +0200

    Disable unused code

--- website/src/lib/config.js
+++ website/src/lib/config.js
@@ -5,14 +5,16 @@
 
 const airtableConfig = {
   endpointUrl: 'https://api.airtable.com',
-  apiKey: process.env.AIRTABLE_API_KEY
+  // somehow the apiKey is not necessary?
+  // apiKey: process.env.AIRTABLE_API_KEY
 };
 
 const sanityConfig = {
   projectId: '0j5eyb7h',
   dataset: 'production',
   apiVersion: '2023-01-16',
-  token: process.env.SANITY_IMPORT,
+  // somehow the apiKey is not necessary?
+  // token: process.env.SANITY_IMPORT,
   useCdn: false
 };
 
